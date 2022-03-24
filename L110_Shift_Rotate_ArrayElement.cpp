8f81bf7bfb799508a41129b321140ad2c27ce999
Bipul Dubey <bipul0873@gmail.com>
2022-03-24 23:25:55 +0530

    Update L110_Shift_Rotate_ArrayElement.cpp

--- L110_Shift_Rotate_ArrayElement.cpp
+++ L110_Shift_Rotate_ArrayElement.cpp
@@ -8,7 +8,7 @@ void display(int arr[],int length){
     cout<<endl;
 }
 
-void RightShift(int arr[],int length,int shift){
+void RightRotate(int arr[],int length,int shift){
     int temp=0,n=0;
     while(n<shift){
         temp=arr[length-1];
@@ -20,7 +20,7 @@ void RightShift(int arr[],int length,int shift){
     }
 }
 
-void LeftShift(int arr[],int length,int shift){
+void LeftRotate(int arr[],int length,int shift){
     int temp=0,n=0;
     while(n<shift){
         temp=arr[0];
@@ -32,16 +32,37 @@ void LeftShift(int arr[],int length,int shift){
     }
 }
 
+void LeftShift(int arr[],int length,int shift){
+    int n=0;
+    while(n<shift){
+        for(int i=0;i<length;i++){
+            arr[i]=arr[i+1];
+        }arr[length-1]=0;
+        n++;
+    }
+}
+
+void RightShift(int arr[],int length,int shift){
+    int n=0;
+    while(n<shift){
+        for(int i=length-1;i>=0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=0;
+        n++;
+    }
+}
+
 int main(){
     int arr[]={1,2,3,4,5,6,7,8,9,10};
     int length=sizeof(arr)/sizeof(arr[0]);
 
     display(arr,length);
-    LeftShift(arr,length,2);
+    LeftRotate(arr,length,2);
     display(arr,length);
     cout<<"\n\n";
     display(arr,length);
-    RightShift(arr,length,2);
+    RightRotate(arr,length,2);
     display(arr,length);
 
     return 0;
